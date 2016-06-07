87f0d429e490e295a7217752fe02621a138e214d
John Baldo <jbaldo@gmail.com>
2016-06-06 17:59:33 -0700

    update deploy scripts with comments

--- deploy_setup.sh
+++ deploy_setup.sh
@@ -1,4 +1,12 @@
+# clone the repo used for staging on GitHub and move into that dir
 git clone git@github.com:openoakland/measure-g-staging.git
 cd measure-g-staging
+
+# have git source repo ignore this new folder
+echo "measure-g-staging/" >> .gitignore
+
+# create an orphan branch with naming required to serve a GitHub project page (gh-pages)
 git checkout --orphan gh-pages
+
+# wipe out any files checked in on the branch
 git rm -rf .
--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,8 +1,16 @@
-
+# move into dir created when running deploy_setup.sh
 cd measure-g-staging
+
+# delete a previously created staging GitHub pages branch on GitHub
 git push origin :gh-pages
+
+# locally remove any existing files on the branch
 git rm -rf .
+
+# compile the source files in the parent folder (measure-g repo)
 harp compile ../ ./
+
+# add and commit all the files on the gh-pages branch, push it up to GitHub for hosting
 git add -A
 git commit -m "$(date +%m-%d-%Y_%H:%M:%S) staging deploy"
 git push origin gh-pages
