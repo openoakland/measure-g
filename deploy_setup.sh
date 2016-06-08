229ebd3496d2cebc962aa0cde05cd4a589125ec9
John Baldo <jbaldo@gmail.com>
2016-06-07 21:01:52 -0700

    remove git ignore step in setup because it's checked into master

--- deploy_setup.sh
+++ deploy_setup.sh
@@ -1,9 +1,5 @@
 # clone the repo used for staging on GitHub and move into that dir
 git clone git@github.com:openoakland/measure-g-staging.git
-
-# have git source repo ignore this new folder
-echo "measure-g-staging/" >> .gitignore
-
 cd measure-g-staging
 
 # create an orphan branch with naming required to serve a GitHub project page (gh-pages)
