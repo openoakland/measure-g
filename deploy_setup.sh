631442147c9b27548ebe1c7e763d5ab1406072f1
John Baldo <1191015+jbaldo@users.noreply.github.com>
2018-07-08 14:21:59 -0700

    getting ready for deployment to GitHub and serving over https

--- deploy_setup.sh
+++ deploy_setup.sh
@@ -7,3 +7,15 @@ git checkout --orphan gh-pages
 
 # wipe out any files checked in on the branch
 git rm -rf .
+
+cd ..
+
+# clone the repo used for prod on GitHub and move into that dir
+git clone git@github.com:openoakland/measure-g-production.git
+cd measure-g-production
+
+# create an orphan branch with naming required to serve a GitHub project page (gh-pages)
+git checkout --orphan gh-pages
+
+# wipe out any files checked in on the branch
+git rm -rf .
